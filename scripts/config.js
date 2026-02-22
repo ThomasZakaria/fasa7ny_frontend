720eebca52b450eee14ae5cd18469d87bcccfbd7
Thomas Zakaria <tt979835@gmail.com>
2026-02-22 23:59:32 +0200

    fix bugs

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,9 @@
 // config.js
-const API_BASE_URL =
+var API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
     : "https://fasa7ny-backend.vercel.app";
 
-// We are using 'window' instead of 'export' to prevent the crash
+// Attach it to window just to be safe
 window.API_BASE_URL = API_BASE_URL;
