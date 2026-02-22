fd8ef8e270a6777f59f11615190b276a573f6587
Thomas Zakaria <tt979835@gmail.com>
2026-02-22 19:53:56 +0200

    Delete Fasa7ny-frontend-main directory

--- Fasa7ny-frontend-main/scripts/script.js
+++
@@ -1,9 +0,0 @@
-function showResults() {
-  const value = document.getElementById("searchInput").value;
-
-  if (value.trim() === "") {
-    alert("Please type a place name first.");
-  } else {
-    alert("Searching for: " + value);
-  }
-}
