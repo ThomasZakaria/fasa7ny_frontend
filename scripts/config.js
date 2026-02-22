7f8b85dd1dac60ab8f932adda9cabbd9532c929e
Thomas Zakaria <tt979835@gmail.com>
2026-02-22 23:08:29 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -5,5 +5,5 @@ const API_BASE_URL =
     ? "http://localhost:3000" // Your local backend
     : "https://fasa7ny-backend.vercel.app"; // Your deployed Vercel backend URL
 
-// Make it available globally to all other scripts
+// We use window instead of export so standard HTML scripts can read it
 window.API_BASE_URL = API_BASE_URL;
