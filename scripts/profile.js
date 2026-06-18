7e2b7fada03f19fd6c61df552f670307b1a62251
Thomas Zakaria <tt979835@gmail.com>
2026-06-18 21:13:46 +0300

    fixes

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,7 +1,7 @@
 // ==========================================
 // Profile Page Logic - Final Fixed Version
 // ==========================================
-const API_BASE_URL = "${API_BASE_URL}";
+const API_BASE_URL = window.API_BASE_URL;
 const DEFAULT_PLACE_IMAGE =
   "https://s7g10.scene7.com/is/image/barcelo/pyramids-of-giza-facts_ancient-pyramids-of-giza?&&fmt=webp-alpha&qlt=75&wid=1300&fit=crop,1";
 
