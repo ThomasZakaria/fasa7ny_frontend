1907acbb88ae18bfbea9a08306c0c49455a13991
Thomas Zakaria <tt979835@gmail.com>
2026-06-18 17:29:38 +0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,14 +1,13 @@
 const getBaseUrl = () => {
   const host = window.location.hostname;
 
-  // لو الموقع شغال محلياً، استخدم اللوكال هوست
+  // لو الموقع شغال محلياً
   if (host === "localhost" || host === "127.0.0.1") {
-    // هنا تكتب الرابط اللوكال مباشرة
     return "http://127.0.0.1:3000/api/v1";
   }
 
-  // لو الموقع شغال لايف (Vercel/Render)
-  return "https://thomaszakaria-fasa7nybackend.vercel.app/api/v1";
+  // الرابط الجديد اللي أخدته من Railway (أضفنا api/v1 في الآخر)
+  return "https://fasa7nybackend-production.up.railway.app/api/v1";
 };
 
 // هنا بنعرف المتغير اللي كل الملفات هتستخدمه
